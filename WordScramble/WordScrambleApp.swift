0e3d5d4378861828b4013077acd7d794764d555f
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-10 15:16:32 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ihor Sukhachov on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
