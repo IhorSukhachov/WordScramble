39779d46402cf78c96f27320ff2961b8c1ab79b3
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-10 17:22:56 +0200

    checking if there was a misspeling

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -21,6 +21,9 @@ struct ContentView: View {
         //cheking word spelling:
         let word = "swift"
         let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
