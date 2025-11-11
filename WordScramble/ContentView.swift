0ebdd73af43d53e1769095a1d58483f27ad60580
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-11 07:43:08 +0200

    making a layout

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-   
+    @State private var usedWordws: [String] = []
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
     var body: some View {
-        
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word:", text: $newWord)
+                    
+                }
+                Section {
+                    ForEach(usedWordws, id: \.self) { word in
+                        Text(word)
+                    }
+                }
+            }
+        }
     }
 }
 
