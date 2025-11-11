499ee067f2fb776274e83cd9d772939aa961c37a
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-11 07:36:58 +0200

    func for testing strings

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,19 +12,6 @@ struct ContentView: View {
     var body: some View {
         
     }
-    
-    func testStrings() {
-        let input = "a b c"
-        let letters = input.components(separatedBy: " ")
-        let letter = letters.randomElement()
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-        //cheking word spelling:
-        let word = "swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
-    }
 }
 
 #Preview {
